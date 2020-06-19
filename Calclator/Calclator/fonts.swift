f2e416724e1c6494fb39ebdde64a3fbe70d31ffc
飯野日向子 <iinohinako@iinohinakonoMacBook-Air.local>
2020-06-19 15:08:41 +0900

    add 002_FirstApp

---
+++ Calclator/Calclator/fonts.swift
@@ -0,0 +1,30 @@
+//
+//  fonts.swift
+//  Calclator
+//
+//  Created by 飯野日向子 on 2020/06/19.
+//  Copyright © 2020 ALJ飯野日向子. All rights reserved.
+//
+
+import UIKit
+
+class fonts: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
